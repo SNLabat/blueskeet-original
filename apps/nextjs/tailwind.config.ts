a4a2d550feb794c31ea74f318b51297b482b378d
SNLabat <snlabat@gmail.com>
2023-05-14 16:54:35 -0500

    tailwind update

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -1,6 +1,7 @@
 const baseConfig = require("@blueskeet/tailwind-config");
 
 module.exports = {
+  purge: ["./components//*.{js,ts,jsx,tsx}", "./pages//*.{js,ts,jsx,tsx}"],
   content: ["./src/**/*.tsx"],
   presets: [baseConfig],
   plugins: [
